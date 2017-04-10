d575d550df9d61bd2a7a1b51ff2c0a8a6c5dc97f
LOVEDXM <275076730@qq.com>
2017-04-10 14:34:35 +0800

    Update async_clien.cpp

--- async_clien.cpp
+++ async_clien.cpp
@@ -1,18 +1,8 @@
-/*******************************************************************************
- * Copyright (c) 2013 Frank Pagliughi <fpagliughi@mindspring.com>
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * and Eclipse Distribution License v1.0 which accompany this distribution.
- *
- * The Eclipse Public License is available at
- *    http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at
- *   http://www.eclipse.org/org/documents/edl-v10.php.
- *
- * Contributors:
- *    Frank Pagliughi - initial implementation and documentation
- *******************************************************************************/
+/*
+*	author:chenxun
+*	time:2017.4.10
+*/
+
 
 #include <iostream>
 #include <cstdlib>
